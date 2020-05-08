b2a8b609a780ec61e51f41f048d26bd35b89c551
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-05-07 21:55:49 -0700

    added unittest function for checking size of df that was created

--- test_data.py
+++ test_data.py
@@ -1,1 +1,26 @@
-ECHO is on.
+import unittest
+
+from early_scripts import csv_script
+
+
+
+class TestCreateDF(unittest.TestCase):
+
+	def test_create_df(self):
+		"""
+		Test that the function returns a df of size (6806, 1982)
+		"""
+		
+		data_path = '.\CollegeScorecard_Raw_Data\CollegeScorecard_Raw_Data\MERGED2018_19_PP.csv'
+		
+		df = csv_script.create_df(data_path)
+		self.assertEqual(df.shape, (6806, 1982))
+	
+if __name__ == '__main__':
+
+	unittest.main()
+		
+
+
+
+
