79394d8c8a43494222ac6eb3be3b36813ee3b9ba
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-05-09 17:10:26 -0700

    fix error with path for test csv data file

--- test_data.py
+++ test_data.py
@@ -4,14 +4,14 @@ from early_scripts import csv_script
 
 
 
-class TestCreateDF(unittest.TestCase):
+class TestCSVScript(unittest.TestCase):
 
-	def test_create_df(self):
+	def test_create_df_of_specific_size(self):
 		"""
 		Test that the function returns a df of size (6806, 1982)
 		"""
 		
-		data_path = '.\CollegeScorecard_Raw_Data\CollegeScorecard_Raw_Data\MERGED2018_19_PP.csv'
+		data_path = 'TESTMERGED2018_19_PP.csv'
 		
 		df = csv_script.create_df(data_path)
 		self.assertEqual(df.shape, (6806, 1982))
