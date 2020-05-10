74ccb43ac9a336ad580e15111eb1706d463fe4b0
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-05-10 13:56:54 -0700

    added renaming function for columns

--- early_scripts/csv_script.py
+++ early_scripts/csv_script.py
@@ -1,22 +1,32 @@
 import pandas as pd
 
 
-college_data_path = '..\CollegeScorecard_Raw_Data\CollegeScorecard_Raw_Data'
+college_data_path = '..\CollegeScorecard_Raw_Data\CollegeScorecard_Raw_Data\MERGED2018_19_PP.csv'
 
 
 ls_of_relevant_clmns = ['INSTNM', 'CITY', 'STABBR', 'ZIP', 'INSTURL',
-						'ADM_RATE', 'SATVR25', 'SATVR75',
-						'SATMT25', 'SATMT75', 'SATWR25', 'SATWR75',
+						'ADM_RATE', 'SATVR25', 'SATVR75', 'SATMT25',
+						'SATMT75', 'SATWR25', 'SATWR75',
 						'SATVRMID', 'SATMTMID', 'ACTCM25', 'ACTCM75',
 						'ACTEN25', 'ACTEN75', 'ACTMT25', 'ACTMT75',
 						'ACTWR25', 'ACTWR75', 'ACTCMMID', 'ACTENMID',
-						'ACTMTMID', 'ACTWRMID', 'SAT_AVG', 'SAT_AVG_ALL',
+						'ACTMTMID', 'ACTWRMID', 'SAT_AVG',
 						'TUITIONFEE_IN', 'TUITIONFEE_OUT',]
+						
+rename_dict = {
+	'INSTNM': 'NAME', 'STABBR': 'STATE', 'INSTURL': 'URL',
+}
 
 
 def create_df(csv_path):
 	
 	df = pd.read_csv(csv_path)
+	
+	return df
+	
+def rename_columns(df):
+	
+	df.rename(columns=rename_dict, inplace=True)
 	
 	return df
 
