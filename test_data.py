15d1d6833d5a2c80966f9f1564bf6fb5440f86f1
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-05-14 12:57:14 -0700

    added function to test whether the retrieve_by_state csv_script function works

--- test_data.py
+++ test_data.py
@@ -31,6 +31,16 @@ class TestCSVScript(unittest.TestCase):
 		df = csv_script.rename_columns(df, rename_dict={'INSTNM': 'NAME'})
 		self.assertFalse(df['NAME'].empty)
 	
+	def test_checks_head_if_is_correct_state(self):
+		"""
+		Test checks if the head of the dataframe contains only
+		the specified state
+		"""
+		df = csv_script.create_df('TESTMERGED2018_19_PP.csv')
+		df = csv_script.rename_columns(df, rename_dict={'STABBR': 'STATE'})
+		df = csv_script.retrieve_by_state(df, 'CA')
+		self.assertEqual(df['STATE'].iloc[0], 'CA')
+	
 if __name__ == '__main__':
 
 	unittest.main()
