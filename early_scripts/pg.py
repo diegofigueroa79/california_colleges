19881ba56ffa5871b7404760ccd8d84ab2dd4bd9
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-05-23 22:51:43 -0700

    modded insert_bulk function to print the sql command that is being sent to the server for debugging

--- early_scripts/pg.py
+++ early_scripts/pg.py
@@ -87,9 +87,9 @@ def insert_bulk_data(ls, credentials):
 			ACTCM75, ACTEN25, ACTEN75, ACTMT25, ACTMT75,\
 			ACTWR25, ACTWR75, ACTCMMID, ACTENMID, ACTMTMID,\
 			ACTWRMID, SAT_AVG, TUITION_IN, TUITION_OUT)\
-			VALUES {}'.format(records_list_template)
-		
-		cursor.execute(insert_query, ls)
+			VALUES {};'.format(records_list_template)
+		print(cursor.mogrify(insert_query, ls))
+		#cursor.execute(insert_query, ls)
 		connection.commit()
 	
 	except (Exception, psycopg2.Error) as error:
